83d2283e53f029d30727aafdf6887d1144fd30a5
Grace Simanullang <gracealvani1@gmail.com>
2025-09-09 14:13:34 +0700

    push
    hapus sidebar admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,19 +10,19 @@
 
                 <div class="dashboard-cards">
                     <div class="dashboard-card">
-                        <h3><i class="fas fa-users"></i> Users</h3>
+                        <h3><i class="fas fa-users"></i> Surat Tugas</h3>
                         <p>Jumlah user terdaftar: 120</p>
                     </div>
                     <div class="dashboard-card">
-                        <h3><i class="fas fa-chart-line"></i> Statistik</h3>
+                        <h3><i class="fas fa-chart-line"></i> Tugas Harian</h3>
                         <p>Kunjungan bulan ini: 3.500</p>
                     </div>
                     <div class="dashboard-card">
-                        <h3><i class="fas fa-wallet"></i> Finance</h3>
+                        <h3><i class="fas fa-wallet"></i> Proposal</h3>
                         <p>Pendapatan bulan ini: Rp 75.000.000</p>
                     </div>
                     <div class="dashboard-card">
-                        <h3><i class="fas fa-tasks"></i> Task</h3>
+                        <h3><i class="fas fa-tasks"></i> Adendum</h3>
                         <p>Proyek aktif: 8</p>
                     </div>
                 </div>
