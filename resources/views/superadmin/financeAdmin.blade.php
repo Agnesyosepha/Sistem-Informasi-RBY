6e8b56e765d8f3b67fa40f407e17255d5be9978c
Grace Simanullang <gracealvani1@gmail.com>
2025-11-27 10:59:39 +0700

    supaa ademindo no cado datta

--- resources/views/superadmin/financeAdmin.blade.php
+++ resources/views/superadmin/financeAdmin.blade.php
@@ -7,6 +7,21 @@
 <h1><i class="fas fa-file-invoice-dollar"></i> Finance Admin</h1>
     <p>Data - data keuangan yang perlu divalidasi.</p>
 
+    <div class="dashboard-cards">
+        <a href="" style="text-decoration:none; color:inherit;">
+            <div class="dashboard-card" style="cursor:pointer;">
+                <h3><i class="fas fa-file-invoice"></i> Invoice</h3>
+                <p><strong>Data Invoice</strong></p>
+            </div>
+        </a>
+        <a href="" style="text-decoration:none; color:inherit;">
+            <div class="dashboard-card" style="cursor:pointer;">
+                <h3><i class="fas fa-users"></i> Staff</h3>
+                <p><strong>Staff</strong></p>
+            </div>
+        </a>
+    </div>
+
     <div class="dashboard-card" style="margin-top:20px;">
         <h3><i class="fas fa-receipt"></i> Data Klaim Perjalanan</h3>
 
