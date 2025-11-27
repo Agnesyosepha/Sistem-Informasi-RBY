59a59cdaea4654f894df2b21c8ad8d408638b3c7
Agnes Yosepha Naibaho <agnesyosephan@gmail.com>
2025-11-27 08:16:26 +0700

    register

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,21 +12,29 @@ class DatabaseSeeder extends Seeder
     {
         User::create([
             'username' => 'user',
+            'email' => 'user@example.com',
+            'divisi' => 'User',
             'password' => Hash::make('123456'),
         ]);
 
         User::create([
             'username' => 'admin',
+            'email' => 'admin@example.com',
+            'divisi' => 'Admin',
             'password' => Hash::make('abcdef'),
         ]);
 
         User::create([
             'username' => 'rby/agnes',
+            'email' => 'agnes@example.com',
+            'divisi' => 'Surveyor',
             'password' => Hash::make('agnes123'),
         ]);
 
         User::create([
             'username' => 'rby/grace',
+            'email' => 'grace@example.com',
+            'divisi' => 'Surveyor',
             'password' => Hash::make('grace123'),
         ]);
     }
