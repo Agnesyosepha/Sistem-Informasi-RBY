b592608fad510535fc4d2163c9381394b4ea8336
Agnes Yosepha Naibaho <agnesyosephan@gmail.com>
2025-11-28 09:38:47 +0700

    akun semua staff

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,5 +37,131 @@ class DatabaseSeeder extends Seeder
             'divisi' => 'Surveyor',
             'password' => Hash::make('grace123'),
         ]);
+
+        //AKUN DIVISI ADMIN
+        User::create([
+            'username' => 'rby/admin/shafa',
+            'email' => 'shafaeriana@gmail.com',
+            'divisi' => 'Admin',
+            'password' => Hash::make('shafa123'),
+        ]);
+        User::create([
+            'username' => 'rby/admin/mieke',
+            'email' => 'mieketarigan@gmail.com',
+            'divisi' => 'Admin',
+            'password' => Hash::make('mieke123'),
+        ]);
+
+        //AKUN DIVISI PENILAI
+        User::create([
+            'username' => 'rby/surveyor/richard',
+            'email' => 'richard.barus33@gmail.com',
+            'divisi' => 'Surveyor',
+            'password' => Hash::make('richard123'),
+        ]);
+        User::create([
+            'username' => 'rby/surveyor/robbi',
+            'email' => 'ragaforex88@gmail.com',
+            'divisi' => 'Surveyor',
+            'password' => Hash::make('robbi123'),
+        ]);
+        User::create([
+            'username' => 'rby/surveyor/firdaus',
+            'email' => 'dausgtg02@gmail.com',
+            'divisi' => 'Surveyor',
+            'password' => Hash::make('firdaus123'),
+        ]);
+        User::create([
+            'username' => 'rby/surveyor/amri',
+            'email' => 'cbnex13@gmail.com',
+            'divisi' => 'Surveyor',
+            'password' => Hash::make('amri123'),
+        ]);
+        User::create([
+            'username' => 'rby/surveyor/fajar',
+            'email' => '20nya.fajartambunan@gmail.com',
+            'divisi' => 'Surveyor',
+            'password' => Hash::make('fajar123'),
+        ]);
+        User::create([
+            'username' => 'rby/surveyor/jasmani',
+            'email' => 'jasmanig97@gmail.com',
+            'divisi' => 'Surveyor',
+            'password' => Hash::make('jasmani123'),
+        ]);
+        User::create([
+            'username' => 'rby/surveyor/santo',
+            'email' => 'antocornelius.g@gmail.com',
+            'divisi' => 'Surveyor',
+            'password' => Hash::make('santo123'),
+        ]);
+        User::create([
+            'username' => 'rby/surveyor/pretty',
+            'email' => 'prettybalerina@icloud.com',
+            'divisi' => 'Surveyor',
+            'password' => Hash::make('pretty123'),
+        ]);
+        User::create([
+            'username' => 'rby/surveyor/benhur',
+            'email' => 'benhurpopuler2002@gmail.com',
+            'divisi' => 'Surveyor',
+            'password' => Hash::make('benhur123'),
+        ]);
+        User::create([
+            'username' => 'rby/surveyor/elma',
+            'email' => 'elmaagnes02@gmail.com',
+            'divisi' => 'Surveyor',
+            'password' => Hash::make('elma123'),
+        ]);
+
+        //AKUN DIVISI EDP
+        User::create([
+            'username' => 'rby/edp/aprilius',
+            'email' => 'rutsahanayasembiring@gmail.com',
+            'divisi' => 'EDP',
+            'password' => Hash::make('aprilius123'),
+        ]);
+        User::create([
+            'username' => 'rby/edp/michael',
+            'email' => 'michaelbp3105@gmail.com',
+            'divisi' => 'EDP',
+            'password' => Hash::make('michael123'),
+        ]);
+        User::create([
+            'username' => 'rby/edp/yohanes',
+            'email' => 'yohaneskrollkoten@gmail.com',
+            'divisi' => 'EDP',
+            'password' => Hash::make('yohanes123'),
+        ]);
+        
+        //AKUN DIVISI REVIEWER
+        User::create([
+            'username' => 'rby/reviewer/mega',
+            'email' => 'MegaPermataSari400@gmail.com',
+            'divisi' => 'Reviewer',
+            'password' => Hash::make('mega123'),
+        ]);
+
+        //AKUN DIVISI FINANCE
+        User::create([
+            'username' => 'rby/finance/amelia',
+            'email' => 'ameliaawandi@gmail.com',
+            'divisi' => 'Finance',
+            'password' => Hash::make('amelia123'),
+        ]);
+        User::create([
+            'username' => 'rby/finance/matta',
+            'email' => 'mattaega1@gmail.com',
+            'divisi' => 'Finance',
+            'password' => Hash::make('matta123'),
+        ]);
+
+        //AKUN DIVISI IT
+        User::create([
+            'username' => 'rby/it/aldi',
+            'email' => 'aldijhont@gmail.com',
+            'divisi' => 'IT',
+            'password' => Hash::make('aldi123'),
+        ]);
     }
 }
