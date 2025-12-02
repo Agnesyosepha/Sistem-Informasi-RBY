99dad2eabb74f25b0e67ae0ca839dff155d15bda
Grace Simanullang <gracealvani1@gmail.com>
2025-12-02 13:58:51 +0700

    - lokasi surve

--- resources/views/layouts/surveyor.blade.php
+++ resources/views/layouts/surveyor.blade.php
@@ -8,12 +8,7 @@
     <p>Ringkasan aktivitas surveyor.</p>
 
     <div class="dashboard-cards">
-        <a href="{{ route('surveyor.lokasisurvei') }}" style="text-decoration:none; color:inherit;">
-            <div class="dashboard-card" style="cursor:pointer;">
-                <h3><i class="fas fa-map-marker-alt"></i> Lokasi Survei</h3>
-                <p><strong>25 Lokasi</strong></p>
-            </div>
-        </a>
+       
         <a href="{{ route('surveyor.updateProyek') }}" style="text-decoration:none; color:inherit;">
             <div class="dashboard-card" style="cursor:pointer;">
                 <h3><i class="fas fa-chart-line"></i> Update Proyek</h3>
--- resources/views/superadmin/surveyorAdmin.blade.php
+++ resources/views/superadmin/surveyorAdmin.blade.php
@@ -8,12 +8,7 @@
 <p>Ringkasan aktivitas surveyor.</p>
 
 <div class="dashboard-cards">
-     <a href="{{ route('superadmin.admin.SAlokasiSurvei') }}" style="text-decoration:none; color:inherit;">
-            <div class="dashboard-card" style="cursor:pointer;">
-                <h3><i class="fas fa-map-marker-alt"></i> Lokasi Survei</h3>
-                <p><strong>25 Lokasi</strong></p>
-            </div>
-        </a>
+     
        <a href="{{ route('superadmin.admin.SAupdateProyek') }}" style="text-decoration:none; color:inherit;">
             <div class="dashboard-card" style="cursor:pointer;">
                 <h3><i class="fas fa-chart-line"></i> Update Proyek</h3>
